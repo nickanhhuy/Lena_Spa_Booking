c764a6ae32ed73968640462f4927062d688518b1
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-17 12:36:31 -0400

    update backend

---
+++ backend_Lena/backend_Lena/src/main/java/com/example/backend_lena/BackendLenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_lena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLenaApplication.class, args);
+    }
+
+}
