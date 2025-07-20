aab24264d1de3c30fce85e562b594527d84d5388
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-20 01:07:26 -0400

    update other services in the controller. REST API <=> Controller <=> Service

--- backend_Lena/backend_Lena/src/main/java/com/example/backend_lena/controller/MainController.java
+++ backend_Lena/backend_Lena/src/main/java/com/example/backend_lena/controller/MainController.java
@@ -17,14 +17,34 @@ public class MainController {
     @Autowired
     BookingService bookingService;
 
-    @GetMapping("/bookings")
+    @GetMapping("/bookings") // get lists of bookings
     public List<BookInfo> getBookingLists() {
         return bookingService.getBookings();
     }
-    @PostMapping("/bookings/addbooking")
+    @PostMapping("/bookings/addbooking") // add a new booking
     public void addBooking(@RequestBody BookInfo booking) {
         bookingService.addOrUpdateBookInfo(booking);
     }
+    @PutMapping("/bookings/{id}/update") // edit the information of the booking
+    public BookInfo updateBooking(@RequestBody BookInfo updated_booking, @PathVariable Long id) {
+        return bookingService.getBookingById(id).map(existing_booking -> {
+            existing_booking.setName(updated_booking.getName());
+            existing_booking.setEmail(updated_booking.getEmail());
+            existing_booking.setPhone(updated_booking.getPhone());
+            existing_booking.setService(updated_booking.getService());
+            existing_booking.setBookingDate(updated_booking.getBookingDate());
+            return bookingService.addOrUpdateBookInfo(existing_booking);
+        }).orElseThrow();
+    }
+    @DeleteMapping("/bookings/{id}") // delete or cancel the booking
+    public void deleteBooking(@PathVariable Long id) {
+        bookingService.deleteById(id);
+    }
+    @GetMapping("/user")
+    public List<BookInfo> getBookingsByEmail(@RequestParam String email) {
+        return bookingService.getByEmail(email);
+    }
+
 
 
 
