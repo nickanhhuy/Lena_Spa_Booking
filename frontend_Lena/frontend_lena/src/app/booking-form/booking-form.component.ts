83de24f8b381506f1936bd7b0af3214f72044095
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-21 10:31:25 -0400

    dateTime paremeter is null after submitted while others worked

--- frontend_Lena/frontend_lena/src/app/booking-form/booking-form.component.ts
+++ frontend_Lena/frontend_lena/src/app/booking-form/booking-form.component.ts
@@ -3,6 +3,7 @@ import { Booking } from '../../booking'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { BookingService } from '../bookingservice.service';
 
 @Component({
   selector: 'app-booking-form',
@@ -21,11 +22,28 @@ export class BookingFormComponent {
     dateTime: ''
   };
   
-  constructor(private router: Router) {}
+  successMessage: string = '';
+
+  constructor(private bookingService : BookingService) {}
 
   submitBooking() {
-    // Logic to handle booking submission
-    console.log('Booking submitted:', this.booking);
-    this.router.navigate(['/confirmation']);
+    this.bookingService.addNewBooking(this.booking).subscribe({
+      next: data => {
+        console.log('Booking submitted:', data);
+        this.successMessage = 'Successfully Booked!';
+        this.booking = {
+          id: 0,
+          name: '',
+          email: '',
+          phone: '',
+          service: '',
+          dateTime: ''
+        };
+      },
+      error: err => {
+        console.error('Booking failed:', err);
+        this.successMessage = 'Something went wrong. Please try again.';
+      }
+    });
   }
 }
