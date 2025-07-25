c452c037369eb73100927aae497a9b7e7ad69199
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-25 15:17:35 -0400

    error with routing to pages

--- frontend_Lena/frontend_lena/src/app/app.routes.ts
+++ frontend_Lena/frontend_lena/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'app-booking-form', component: BookingFormComponent},
+    {path: 'app-booking-list', component: BookingListComponent},
+];
