b72331f67f048e4dc107ffe79ca237355d3d7131
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-26 00:29:25 -0400

    fixed routes by adding provideRouter in main.ts

--- frontend_Lena/frontend_lena/src/app/app.component.ts
+++ frontend_Lena/frontend_lena/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 import { BookingListComponent } from './booking-list/booking-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BookingFormComponent, BookingListComponent],
+  standalone: true,
+  imports: [RouterOutlet, BookingFormComponent, BookingListComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend_lena';
+  title = 'Lena Beauty Spa';
 }
