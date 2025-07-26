eee5b47d07002ab7446ba875b4198afa880ba4d7
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-25 20:06:02 -0400

    fixed routes by adding provideRouter in main.ts

--- frontend_Lena/frontend_lena/src/app/app.routes.ts
+++ frontend_Lena/frontend_lena/src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 import { BookingListComponent } from './booking-list/booking-list.component';
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
+    {path: 'app-home', component: HomeComponent},
     {path: 'app-booking-form', component: BookingFormComponent},
     {path: 'app-booking-list', component: BookingListComponent},
 ];
